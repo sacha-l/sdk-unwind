6d9db55c56643137b64e078f4389dd61cedb30c1
Sacha Lansky <sacha@parity.io>
2023-10-17 20:16:49 +0200

    add super-basic-pallet

---
+++ sdk-unwind-workspace/super-basic-pallet/src/lib.rs
@@ -0,0 +1,92 @@
+//! # Super basic Pallet
+//!
+//! A pallet in `dev_mode` with minimal functionality to demonstrate basic patterns for writing a FRAME pallet.
+//! 
+//! These include:
+//! - Different pallet parts such as: the `Config` trait, storage, genesis config, genesis build, and calls.
+//! - Using a mock runtime to write unit tests for the callable logic a pallet exposes.
+
+// We make sure this pallet uses `no_std` for compiling to Wasm.
+#![cfg_attr(not(feature = "std"), no_std)]
+
+// Re-export pallet items so that they can be accessed from the crate namespace.
+pub use pallet::*;
+
+// Used for safe math.
+use sp_arithmetic::traits::Saturating;
+
+/// ** TESTING MODULE **
+#[cfg(test)]
+mod tests;
+
+#[frame_support::pallet(dev_mode)]
+pub mod pallet {
+	// Import various useful types required by all FRAME pallets.
+	use super::*;
+	use frame_support::pallet_prelude::*;
+	use frame_system::pallet_prelude::*;
+
+	// The `Pallet` struct serves as a placeholder to implement traits, methods and dispatchables in
+	// this pallet.
+	#[pallet::pallet]
+	pub struct Pallet<T>(_);
+
+	/// ** PALLET CONFIGURATION TRAIT **
+	#[pallet::config]
+	pub trait Config: pallet_balances::Config + frame_system::Config {}
+
+	/// ** STORAGE **
+    /// A single value storage item that stores some Balance.
+	#[pallet::storage]
+	#[pallet::getter(fn dummy)]
+	pub(super) type Dummy<T: Config> = StorageValue<_, T::Balance>;
+
+	/// ** GENESIS **
+	// A way to configure the genesis state of your pallet.
+	#[pallet::genesis_config]
+	#[derive(frame_support::DefaultNoBound)]
+	pub struct GenesisConfig<T: Config> {
+		pub dummy: T::Balance,
+	}
+
+	// A way to build the genesis state of your pallet.
+	#[pallet::genesis_build]
+	impl<T: Config> BuildGenesisConfig for GenesisConfig<T> {
+		fn build(&self) {
+			Dummy::<T>::put(&self.dummy);
+		}
+	}
+
+	/// ** PALLET CALL BLOCK **
+    /// Your pallet's callable functions go here.
+    /// Note: because we are using dev_mode, we don't need to care about weights or call indices.
+	#[pallet::call]
+	impl<T: Config> Pallet<T> {
+		pub fn accumulate_dummy(origin: OriginFor<T>, increase_by: T::Balance) -> DispatchResult {
+			// check that the caller is signed
+			let _sender = ensure_signed(origin)?;
+
+			// // demo of None value in storage, the default value for the storage item
+			// let current_value_in_dummy = Self::dummy();
+			// assert_eq!(current_value_in_dummy, None);
+
+			Dummy::<T>::mutate(|dummy| {
+				// Using `saturating_add` instead of a regular `+` to avoid overflowing
+				let new_dummy = dummy.map_or(increase_by, |d| d.saturating_add(increase_by));
+				*dummy = Some(new_dummy);
+			});
+
+			Ok(())
+		}
+
+		/// A function that deletes the storage of Dummy, that can only be called by some root
+		/// origin.
+		pub fn kill_dummy(origin: OriginFor<T>) -> DispatchResult {
+			let _caller = ensure_root(origin)?;
+
+			Dummy::<T>::kill();
+
+			Ok(())
+		}
+	}
+}
---
+++ sdk-unwind-workspace/super-basic-pallet/src/tests.rs
@@ -0,0 +1,123 @@
+// This file is part of Substrate.
+
+// Copyright (C) Parity Technologies (UK) Ltd.
+// SPDX-License-Identifier: Apache-2.0
+
+//! Mock runtime environment and tests for the Super Basic pallet.
+
+use crate::*;
+use frame_support::{
+	assert_ok,
+	traits::ConstU64,
+};
+use sp_core::H256;
+// The testing primitives are very useful for avoiding having to work with signatures
+// or public keys. `u64` is used as the `AccountId` and no `Signature`s are required.
+use sp_runtime::{
+	traits::{BlakeTwo256, IdentityLookup},
+	BuildStorage,
+};
+// Reexport crate as its pallet name for construct_runtime.
+use crate as pallet_example_super_basic;
+
+type Block = frame_system::mocking::MockBlock<Test>;
+
+// For testing the pallet, we construct a mock runtime.
+frame_support::construct_runtime!(
+	pub enum Test
+	{
+		System: frame_system::{Pallet, Call, Config<T>, Storage, Event<T>},
+		Balances: pallet_balances::{Pallet, Call, Storage, Config<T>, Event<T>},
+		Example: pallet_example_super_basic::{Pallet, Call, Storage, Config<T>},
+	}
+);
+
+impl frame_system::Config for Test {
+	type BaseCallFilter = frame_support::traits::Everything;
+	type BlockWeights = ();
+	type BlockLength = ();
+	type DbWeight = ();
+	type RuntimeOrigin = RuntimeOrigin;
+	type Nonce = u64;
+	type Hash = H256;
+	type RuntimeCall = RuntimeCall;
+	type Hashing = BlakeTwo256;
+	type AccountId = u64;
+	type Lookup = IdentityLookup<Self::AccountId>;
+	type Block = Block;
+	type RuntimeEvent = RuntimeEvent;
+	type BlockHashCount = ConstU64<250>;
+	type Version = ();
+	type PalletInfo = PalletInfo;
+	type AccountData = pallet_balances::AccountData<u64>;
+	type OnNewAccount = ();
+	type OnKilledAccount = ();
+	type SystemWeightInfo = ();
+	type SS58Prefix = ();
+	type OnSetCode = ();
+	type MaxConsumers = frame_support::traits::ConstU32<16>;
+}
+
+impl pallet_balances::Config for Test {
+	type MaxLocks = ();
+	type MaxReserves = ();
+	type ReserveIdentifier = [u8; 8];
+	type Balance = u64;
+	type DustRemoval = ();
+	type RuntimeEvent = RuntimeEvent;
+	type ExistentialDeposit = ConstU64<1>;
+	type AccountStore = System;
+	type WeightInfo = ();
+	type FreezeIdentifier = ();
+	type MaxFreezes = ();
+	type RuntimeHoldReason = ();
+	type MaxHolds = ();
+}
+
+// Implement the pallet's configuration trait for the mock runtime.
+impl Config for Test {}
+
+// This function basically just builds a genesis storage key/value store according to
+// our desired mockup.
+pub fn new_test_ext() -> sp_io::TestExternalities {
+	let t = RuntimeGenesisConfig {
+		// We use default for brevity, but you can configure as desired if needed.
+		system: Default::default(),
+		balances: Default::default(),
+		example: pallet_example_super_basic::GenesisConfig { dummy: 42 },
+	}
+	.build_storage()
+	.unwrap();
+	t.into()
+}
+
+#[test]
+fn genesis_storage_works() {
+	new_test_ext().execute_with(|| {
+		// check that our genesis build worked
+		assert_eq!(Example::dummy(), Some(42));
+	});
+}
+
+#[test]
+fn accumulate_dummy_works() {
+	new_test_ext().execute_with(|| {
+		let genesis_val = 42;
+		let increment_by = 100;
+
+		// check that accumulate works when we have Some value in Dummy already
+		assert_ok!(Example::accumulate_dummy(RuntimeOrigin::signed(1), increment_by));
+		assert_eq!(Example::dummy(), Some(genesis_val + increment_by));
+	});
+}
+
+#[test]
+fn kill_dummy_works() {
+	new_test_ext().execute_with(|| {
+        // we know there is the value Som(42) in storage
+		assert_eq!(Example::dummy(), Some(42));
+        // construct and dispatch the call with root origin
+        assert_ok!(Example::kill_dummy(RuntimeOrigin::root()));
+		assert_eq!(Example::dummy(), None);
+	});
+}
