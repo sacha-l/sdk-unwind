0d3d637b0604616db78b46101988792ec84a684e
Sacha Lansky <sacha@parity.io>
2023-10-18 18:10:08 +0200

    make mock compile

--- sdk-unwind-workspace/multi-pallet-crate/src/membership.rs
+++ sdk-unwind-workspace/multi-pallet-crate/src/membership.rs
@@ -33,44 +33,109 @@ pub mod pallet {
 
 	// The pallet's configuration trait.
 	#[pallet::config]
-	pub trait Config: frame_system::Config {}
-
-    #[pallet::storage]
-    pub (super) type Members<T: Config> = StorageValue<_, BoundedVec<(T::AccountId, u32, u8), ConstU32<100>>>;
-
-    #[pallet::call]
-    impl<T: Config> Pallet<T> {
-
-        /// Adds a member to the vec in storage.
-        pub fn add_member(
-            origin: OriginFor<T>,
-            member: T::AccountId,
-            class_of: u32,
-            emoji: u8,
-        ) -> DispatchResult {
-
-            // check the origin 
-            ensure_signed(origin)?;
-
-            // write to Members storage
-            let _ = Self::do_update_members(member, class_of, emoji);
-
-            Ok(())
-        }
+	pub trait Config: frame_system::Config {
+        /// This is the max amount of members allowed.
+        type MaxMembers: Get<u32>;
     }
+
+	#[pallet::storage]
+	pub type Members<T: Config> =
+		StorageValue<_, BoundedVec<(T::AccountId, u32, u8), T::MaxMembers>>;
+
+	#[pallet::call]
+	impl<T: Config> Pallet<T> {
+		/// Adds a member to the vec in storage.
+		pub fn add_member(
+			origin: OriginFor<T>,
+			member: T::AccountId,
+			class_of: u32,
+			emoji: u8,
+		) -> DispatchResult {
+			// check the origin
+			ensure_signed(origin)?;
+
+			// write to Members storage
+			let _ = Self::do_update_members(member, class_of, emoji);
+
+			Ok(())
+		}
+	}
 }
 
 impl<T: Config> Pallet<T> {
+	/// Writes new Member to storage
+	pub fn do_update_members(member: T::AccountId, class: u32, emoji: u8) -> DispatchResult {
+		let member = (member, class, emoji);
 
-    /// Writes new Member to storage
-    pub fn do_update_members(member: T::AccountId, class: u32, emoji: u8) -> DispatchResult {
+		// write storage update here
+		Members::<T>::try_append(member).map_err(|_| "max members exceeded")?;
 
-        let member = (member, class, emoji);
+		Ok(())
+	}
+}
 
-        // write storage update here
-        Members::<T>::try_append(member).map_err(|_| "max members exceeded")?;
+/// ** TESTING MODULE **
+#[cfg(test)]
+mod tests {
 
-        Ok(())
+	//! Mock runtime environment and tests for the Super Basic pallet.
+	use crate::{
+        dummy_pallet::pallet::{self as pallet_dummy},
+        membership::pallet::{self as pallet_membership}
+    };
+	use frame_support::{assert_ok, traits::ConstU64};
+	use sp_core::H256;
+    use sp_core::ConstU32;
+
+	// The testing primitives are very useful for avoiding having to work with signatures
+	// or public keys. `u64` is used as the `AccountId` and no `Signature`s are required.
+	use sp_runtime::{
+		traits::{BlakeTwo256, IdentityLookup},
+		BuildStorage,
+	};
+
+	type Block = frame_system::mocking::MockBlock<Runtime>;
+
+	// For testing the pallet, we construct a mock runtime.
+	frame_support::construct_runtime!(
+		pub enum Runtime
+		{
+			System: frame_system::{Pallet, Call, Config<T>, Storage, Event<T>},
+            Dummy: pallet_dummy,
+            Members: pallet_membership,
+		}
+	);
+
+	impl frame_system::Config for Runtime {
+		type BaseCallFilter = frame_support::traits::Everything;
+		type BlockWeights = ();
+		type BlockLength = ();
+		type DbWeight = ();
+		type RuntimeOrigin = RuntimeOrigin;
+		type Nonce = u64;
+		type Hash = H256;
+		type RuntimeCall = RuntimeCall;
+		type Hashing = BlakeTwo256;
+		type AccountId = u64;
+		type Lookup = IdentityLookup<Self::AccountId>;
+		type Block = Block;
+		type RuntimeEvent = RuntimeEvent;
+		type BlockHashCount = ConstU64<250>;
+		type Version = ();
+		type PalletInfo = PalletInfo;
+		type AccountData = ();
+		type OnNewAccount = ();
+		type OnKilledAccount = ();
+		type SystemWeightInfo = ();
+		type SS58Prefix = ();
+		type OnSetCode = ();
+		type MaxConsumers = frame_support::traits::ConstU32<16>;
+	}
+
+	// Implement the pallet's configuration trait for the mock runtime.
+	impl pallet_dummy::Config for Runtime {}
+    
+    impl pallet_membership::Config for Runtime {
+        type MaxMembers = ConstU32<100>;
     }
- 
 }
